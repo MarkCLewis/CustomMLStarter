d17851b21385f18fb681157005397fa9cea56916
Mark Lewis <mlewis@trinity.edu>
2019-09-23 18:52:42 -0500

    Updated comments on Associations.

--- src/main/scala/customml/association/Associations.scala
+++ src/main/scala/customml/association/Associations.scala
@@ -1,8 +1,22 @@
 package customml.association
 
 object Associations {
+  /**
+   * Represents an association of the form antecedent -> consequent.
+   */
   case class Association[A](antecedent: Set[A], consequent: Set[A])
-  def apriori[A](groupings: Seq[Set[A]], desiredGroupSize: Int, minSupport: Double, minConfidence: Double): Set[Association[A]] = {
+
+  /**
+   * Use the apriori algorithm to calculate the appropriate associations for the specified groupings. The groupings are the
+   * items that all come together, for example, orders from customers. We are looking for associations that have a particular
+   * number of elements in the antecedent and consequent combined. Every association returned needs to have the specified minimum
+   * support and confidence.
+   * @param groupings The groups of elements that you should find associations for.
+   * @param desiredAssociationSize The number of elements in the associations that you should be building.
+   * @param minSupport The minimum required support for all the desired associations.
+   * @param minConfidence The minimum required confidence for all the desired associations.
+   */
+  def apriori[A](groupings: Seq[Set[A]], desiredAssociationSize: Int, minSupport: Double, minConfidence: Double): Set[Association[A]] = {
     ???
   }
 }
